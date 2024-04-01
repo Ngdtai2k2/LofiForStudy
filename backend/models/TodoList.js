7f80d311bff4d5b96fd367338a473312eea8221e
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-01 18:43:07 +0700

    add plugin paginate for todo list

--- backend/models/TodoList.js
+++ backend/models/TodoList.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
 const todoListSchema = new mongoose.Schema(
   {
@@ -36,5 +37,7 @@ const todoListSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+todoListSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model("TodoList", todoListSchema);
