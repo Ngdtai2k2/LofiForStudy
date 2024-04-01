7963fc85cd022400eebb40481b3951a3f3418475
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-01 18:48:21 +0700

    handel upload file to cloudinary

---
+++ backend/controllers/uploadMediaController.js
@@ -0,0 +1,50 @@
+const cloudinary = require("../configs/cloudinary");
+const mediaController = require("./mediaController");
+
+const uploadMedia = {
+  uploadImage: async (req, res) => {
+    try {
+      const data = await cloudinary.uploader.upload(req.file.path);
+      const { secure_url, public_id } = data;
+      const newMedia = await mediaController.createMedia({
+        url: secure_url,
+        type: 0,
+        cloudinary_id: public_id,
+      });
+      return newMedia || null;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      return null;
+    }
+  },
+
+  uploadAudio: async (req, res) => {
+    try {
+      const data = await cloudinary.uploader.upload(req.file.path, {
+        resource_type: 'auto'
+      });
+      const { secure_url, public_id } = data;
+      const newMedia = await mediaController.createMedia({
+        url: secure_url,
+        type: 1,
+        cloudinary_id: public_id,
+      });
+      return newMedia || null;
+    } catch (error) {
+      console.error("Error uploading audio:", error);
+      return null;
+    }
+  },
+
+  deleteFile: async (public_id) => {
+    try {
+      await cloudinary.uploader.destroy(public_id);
+      return true;
+    } catch (error) {
+      console.error("Error deleting file:", error);
+      return false;
+    }
+  },
+};
+
+module.exports = uploadMedia;
