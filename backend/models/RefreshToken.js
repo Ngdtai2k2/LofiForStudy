c26e8e66963d00f9dd67f192be795f3542f7aaec
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-01 16:16:34 +0700

    create model refresh token

---
+++ backend/models/RefreshToken.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const refreshSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    device: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("RefreshToken", refreshSchema);
