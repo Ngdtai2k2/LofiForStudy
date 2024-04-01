e43e3e53e44ae41a27984fb55d7012cb07b8a0af
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-01 18:43:19 +0700

    configuration multer

---
+++ backend/configs/multer.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const path = require("path");
+
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (
+      ext !== ".mp3" &&
+      ext !== ".wav" &&
+      ext !== ".wma" &&
+      ext !== ".jpeg" &&
+      ext !== ".jpg" &&
+      ext !== ".png" &&
+      ext !== ".gif"
+    ) {
+      cb(new Error("File type is not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
