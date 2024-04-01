1a655a0b56ea0af2fffa6e0c8ba6b8d4cb50b297
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-01 12:31:14 +0700

    connect mongodb and server

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOOSE_DB)
+  .then(() => {
+    console.log(">>> Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error(">>> Error connecting to MongoDB:", error);
+  });
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log(">>> Server running on port 8000!");
+});
