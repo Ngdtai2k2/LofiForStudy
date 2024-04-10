9664c66944df3618b27de86b830b9ddaad113f51
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-10 14:08:29 +0700

    create artist information component

---
+++ frontend/src/components/ArtistInformation/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import Link from '@mui/material/Link';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+import BoxOverlay from '../BoxOverlay';
+import { YOUTUBE_URL } from '../../constants/constant';
+export default function ArtistInformation({
+  channelId,
+  channelTitle,
+  thumbnailsHigh,
+}) {
+  return (
+    <BoxOverlay sx={{ height: '100%' }}>
+      <Link
+        className="flex-column-center"
+        href={`${YOUTUBE_URL}channel/${channelId}`}
+        underline="none"
+        maxWidth={60}
+      >
+        <Avatar variant="rounded" src={thumbnailsHigh} alt={`Profile picture of ${channelTitle}`} />
+        <Marquee direction="left" speed={10} delay={2}>
+          <Typography variant="caption" color="#ffffff">
+            {channelTitle}&nbsp;
+          </Typography>
+        </Marquee>
+      </Link>
+    </BoxOverlay>
+  );
+}
