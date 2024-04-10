eb6c46eb533fa54e09c072344196b7e13e80c011
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-04-10 10:06:23 +0700

    add variable for API YOUTUBE

--- frontend/src/constants/constant.js
+++ frontend/src/constants/constant.js
@@ -7,3 +7,9 @@ export const toastTheme = {
 };
 
 export const BaseApi = process.env.REACT_APP_BASE_URL;
+
+export const YOUTUBE_URL = process.env.REACT_APP_YOUTUBE_URL;
+
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+export const YOUTUBE_API_URL = process.env.REACT_APP_YOUTUBE_API_URL;
